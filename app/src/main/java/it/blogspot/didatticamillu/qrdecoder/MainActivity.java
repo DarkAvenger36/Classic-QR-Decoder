e88be1499b798c19ca5e13cb02fd453ef57c99b9
Lorenzo <lorenzo.millucci@outlook.com>
2014-08-11 17:54:59 +0200

    Versione iniziale

---
+++ app/src/main/java/it/blogspot/didatticamillu/qrdecoder/MainActivity.java
@@ -0,0 +1,219 @@
+package it.blogspot.didatticamillu.qrdecoder;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.net.URISyntaxException;
+
+import main.java.com.google.zxing.BinaryBitmap;
+import main.java.com.google.zxing.ChecksumException;
+import main.java.com.google.zxing.FormatException;
+import main.java.com.google.zxing.NotFoundException;
+import main.java.com.google.zxing.RGBLuminanceSource;
+import main.java.com.google.zxing.Reader;
+import main.java.com.google.zxing.Result;
+import main.java.com.google.zxing.common.HybridBinarizer;
+import main.java.com.google.zxing.qrcode.QRCodeReader;
+
+
+public class MainActivity extends Activity {
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
+    private final int FILE_SELECT_CODE = 0;
+    private static final int TAKE_PICTURE = 1;
+    private TextView message = null;
+    private ImageView imageView = null;
+    private static Uri imageUri;
+    private static final int SCALEW = 800;
+    private static final int SCALEH = 600;
+    private Bitmap bmp;
+
+
+    public void openFilePicker(View view) {
+
+
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("*/*");
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+            try {
+                startActivityForResult(
+                        Intent.createChooser(intent, "Select a image to decode"),
+                        FILE_SELECT_CODE);
+            } catch (android.content.ActivityNotFoundException ex) {
+                // Potentially direct the user to the Market with a Dialog
+                Toast.makeText(this, R.string.activity_not_found,
+                        Toast.LENGTH_SHORT).show();
+            }
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case FILE_SELECT_CODE:
+                if (resultCode == RESULT_OK) {
+                    // Get the Uri of the selected file
+                    Uri uri = data.getData();
+                    Log.d(LOG_TAG, "File Uri: " + uri.toString());
+                    try {
+                        // Get the path
+                        String path = FileUtils.getPath(this, uri);
+                        Log.d(LOG_TAG, "File Path: " + path);
+                        decodeImage(path);
+                    }catch (URISyntaxException e){
+                        e.printStackTrace();
+                    }
+
+                }
+                break;
+            case TAKE_PICTURE:
+                if (resultCode == Activity.RESULT_OK) {
+                    Uri selectedImage = imageUri;
+                    //Log.d(LOG_TAG, "Uri in resoult = " + selectedImage.toString());
+                    decodeImage(selectedImage.getPath());
+                }
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    public static class Global{
+        public static  String text = null;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        message = (TextView)findViewById(R.id.myText);
+        imageView = (ImageView)findViewById(R.id.imageView);
+        message.setMovementMethod(new ScrollingMovementMethod());
+
+    }
+
+
+    public void decodeImage(String path){
+        message.setText("");
+        Global.text= "";
+
+        // Get the dimensions of the View
+        int targetW = SCALEW;
+        int targetH = SCALEH;
+
+        Log.d(LOG_TAG, "W= " + targetW + " H= " + targetH);
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(path, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Log.d(LOG_TAG, "PATH = " + path);
+        bmp = BitmapFactory.decodeFile(path, bmOptions);
+        Log.d(LOG_TAG, "dopo il decode");
+
+        int width = bmp.getWidth();
+        int height = bmp.getHeight();
+        int[] pixels = new int[width * height];
+        bmp.getPixels(pixels, 0 , width, 0, 0, width, height);
+
+        RGBLuminanceSource src = new RGBLuminanceSource(width, height, pixels);
+
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(src));
+        Reader reader = new QRCodeReader();
+
+        Log.d(LOG_TAG, "prima del try");
+
+        try{
+            Result result = reader.decode(bitmap);
+            Global.text = result.getText();
+
+            message.setText(Global.text);
+            Log.d(LOG_TAG, "messaggio = " + Global.text);
+        }catch (NotFoundException e){
+            e.printStackTrace();
+        }catch (ChecksumException ce){
+            ce.printStackTrace();
+        }catch (FormatException fe){
+            fe.printStackTrace();
+        }
+        imageView.setImageBitmap(bmp);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_photo) {
+            takePhoto();
+            return true;
+        }
+        if (id == R.id.action_choose_file) {
+            openFilePicker(null);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+    public void takePhoto() {
+        String file_path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/QRDECODE";
+        File dir = new File(file_path);
+        if (!dir.exists()) {
+            dir.mkdirs();
+            Log.d(LOG_TAG, "Sono nell'if");
+        }
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        File photo = new File(file_path,  "Pic.jpg");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,
+                Uri.fromFile(photo));
+        imageUri = Uri.fromFile(photo);
+        Log.d(LOG_TAG, "URI = " + imageUri.toString());
+        startActivityForResult(intent, TAKE_PICTURE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        message.setText(Global.text);
+        imageView.setImageBitmap(bmp);
+    }
+}
